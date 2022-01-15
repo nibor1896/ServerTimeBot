4ebb0961f2ecaa63fdc2a85a17533fe47dcb657c
nibor1896 <56588205+nibor1896@users.noreply.github.com>
2022-01-15 02:55:49 +0100

    Update Util.template.js

--- util/Util.template.js
+++ util/Util.template.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	title: "Server Time",
 	name: "FFXIV Server Time",
-	TOKEN: "Your token here",
+	TOKEN: "OTMxNzI1MzY4ODg3NDk2NzM0.YeIm3g.WcnsOT_oggZOh8OsdZO0IT7BNfY",
 	PREFIX: "?"
 	
 }
